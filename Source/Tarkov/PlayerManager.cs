e58a8b8ffe803366707f0b42cb3dcfbd38da6e93
Gardenmasterxx <counterfreak@gmx.net>
2024-11-01 22:20:27 +0100

    Update PlayerManager.cs

--- Source/Tarkov/PlayerManager.cs
+++ Source/Tarkov/PlayerManager.cs
@@ -1,4 +1,4 @@
-﻿using Offsets;
+using Offsets;
 using System.Numerics;
 
 namespace eft_dma_radar
@@ -315,7 +315,7 @@ namespace eft_dma_radar
                     entries.Add(new ScatterWriteDataEntry<float>(this._physical + Offsets.Physical.FallDamageMultiplier, 1f));
                 }
 
-                if (this._physicalCondition != 0)
+            if (this._physicalCondition != 0 && (this._physicalCondition != 3592 && this._physicalCondition != 1544))
                     entries.Add(new ScatterWriteDataEntry<int>(this._movementContext + Offsets.MovementContext.PhysicalCondition, 0));
 
                 if (this._speedLimit != 1)
